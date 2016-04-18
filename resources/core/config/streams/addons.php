0196209c87c710de9435e37a05e270ea043236c1
Sam Likins <sam.likins@wsi-services.com>
2016-04-18 16:00:40 -0400

    Initial project commit.

---
+++ resources/core/config/streams/addons.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Eager Addons
+    |--------------------------------------------------------------------------
+    |
+    | Eager loaded addons are registered first and can be defined
+    | here by specifying their relative path to the addon's root.
+    |
+    */
+
+    'eager' => [
+        'core/anomaly/redirects-module',
+        'core/anomaly/sitemap-extension',
+        'core/anomaly/pages-module'
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Deferred Addons
+    |--------------------------------------------------------------------------
+    |
+    | Deferred addons are registered last and can be defined
+    | here by specifying their relative path to the addon's root.
+    |
+    */
+
+    'deferred' => []
+
+];
